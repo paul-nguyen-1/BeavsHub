0227ee5cad2ccc92e406595fd5a97c9f7c460654
paul-nguyen96 <paul121596@icloud.com>
2024-12-08 22:36:22 -0600

    set up jest workflow

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+    rootDir: './apps/api', 
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    testRegex: '.spec.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s', '!**/node_modules/**'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
+  };
+  
