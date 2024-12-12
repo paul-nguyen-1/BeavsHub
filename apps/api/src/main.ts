6a369647b8147265f6f2d380cd39fa3bf0a878e5
paul-nguyen96 <paul121596@icloud.com>
2024-12-12 13:02:41 -0600

    update env variable name

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.BEAVS_HUB_BACKEND_ENDPOINT],
+    origin: ['http://localhost:3000', process.env.PORT],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
