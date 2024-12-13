fcd224d33075f6ace6dd2cdc89400cb72152a6a1
paul-nguyen96 <paul121596@icloud.com>
2024-12-12 18:58:21 -0600

    test hotfix -- add sheet credentials

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const fs = require('fs');
+  if (process.env.GOOGLE_API_CREDENTIALS) {
+    const credentialsPath = './sheets_credentials.json';
+    fs.writeFileSync(credentialsPath, process.env.GOOGLE_API_CREDENTIALS);
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
+  }
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
